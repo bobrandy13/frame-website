b3c7ff7c45a4d697f73099276f0fc050e5b81cbf
Kevin Huang <khuang891@gmail.com>
2023-03-07 21:20:47 +1100

    vercal deployment changes

--- pages/products/[products].tsx
+++ pages/products/[products].tsx
@@ -15,15 +15,31 @@ const Product = ({data}: {data: ProductType}) => {
   return (
     <>
       <div>
-        <div className="relative left-11 h-96 w-96 min-h-screen flex items-center">
-          <div className="h-4/5 w-72 relative">
+        {/* <div className="relative left-11 h-96 w-1/2 min-h-screen flex items-center">
+          <div className="relative h-4/5 w-screen m-2 ">
             <Image
               src={individualProduct.Image}
               alt="product image"
               fill
-              style={{ objectFit: "cover" }}
+              style={{ objectFit: "contain" }}
             />
           </div>
+        </div>
+        <div className="inline float-right absolute top-36 right-80"><h1 className="text-3xl font-bold">{individualProduct.name}</h1></div> */}
+        <div className="min-h-screen flex flex-col flex-wrap justify-center md:flex-row">
+          <div className="container-1 grow-[2] bg-red-100 m-2 flex items-center justify-center">
+            <div className="relative w-4/5 h-screen">
+              <Image
+                src={individualProduct.Image}
+                alt="product image"
+                fill
+                style={{ objectFit: "contain" }}
+              />
+            </div>
+          </div>
+          <div className="container-2 bg-indigo-200 m-2 flex-grow text-center flex justify-center items-center flex-wrap">
+            <h1 className="text-3xl">{individualProduct.name}</h1>
+          </div>  
         </div>
       </div>
     </>
